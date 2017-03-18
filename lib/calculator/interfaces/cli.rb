e0fa86ef73f14cf4b4a3cf12d080d2aedb1a122d
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-18 02:02:42 -0300

    Check if the gets is not nil

--- lib/calculator/interfaces/cli.rb
+++ lib/calculator/interfaces/cli.rb
@@ -2,7 +2,7 @@ module Interfaces
   class CLI < BaseInterface
     def start
       puts 'Starting calculator.'
-      while input = $stdin.gets
+      while input = $stdin.gets&.chomp
         puts engine.process_input(Input.new(input))
       end
     end
