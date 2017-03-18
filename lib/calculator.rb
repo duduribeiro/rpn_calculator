98ccfca8c51a225248ca75ff643d8b4eb744cbf3
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-18 01:55:21 -0300

    Require the input class on the main calculator file

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,7 @@
 require 'calculator/version'
 require 'calculator/errors/invalid_calculation'
 require 'calculator/runner'
+require 'calculator/input'
 require 'calculator/engine'
 require 'calculator/interface'
 
