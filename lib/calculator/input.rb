d1c1f5748e4a78320e65d06f74c5ba46152d895a
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-18 02:39:53 -0300

    Method to check if an input is an operator

--- lib/calculator/input.rb
+++ lib/calculator/input.rb
@@ -3,11 +3,23 @@ class Input
     @input = input
   end
 
+  def operator_key
+    @input
+  end
+
+  def operator?
+    Operator.exists?(@input)
+  end
+
   def value
     @input.to_i
   end
 
   def value?
     /\A\d+\z/.match(@input)
+  end
+
+  def to_s
+    @input
   end
 end
