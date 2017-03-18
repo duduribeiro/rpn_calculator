b42d6a6fc040d8a9c07667e27ba9702fcbb3b75b
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-18 03:11:44 -0300

    Append a > on cli output

--- lib/calculator/interfaces/cli.rb
+++ lib/calculator/interfaces/cli.rb
@@ -3,10 +3,16 @@ module Interfaces
     def start
       puts 'Starting calculator.'
       while stdin = $stdin.gets&.chomp
-        input = Input.new(stdin)
-        puts input.to_s
-        result = engine.process_input(input)
-        puts result if result
+        begin
+          input = Input.new(stdin)
+          result = engine.process_input(input)
+          puts "> #{input.to_s}"
+          puts "> #{result}" if result
+        rescue Errors::InvalidCalculation => e
+          puts "💥 invalid calculation. #{e.message}"
+        rescue Errors::InvalidOperator
+          puts "> This isn't a valid operator 😔"
+        end
       end
     end
   end
