27c31d8265b9243056a88f436ca259f43041ca82
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-18 15:00:49 -0300

    Tests for CLI when it receives an invalid operator

--- spec/lib/calculator/interfaces/cli_spec.rb
+++ spec/lib/calculator/interfaces/cli_spec.rb
@@ -28,6 +28,21 @@ module Calculator
           expect { subject }.not_to raise_error SystemExit
         end
       end
+
+      context 'with invalid calculation' do
+        before(:each) do
+          allow(engine).to receive(:process_input).and_raise(Errors::InvalidOperator)
+          allow($stdin).to receive(:gets).and_return('1', nil)
+        end
+
+        it 'displays that it is an invalid operator' do
+          expect { subject }.to output(['Starting calculator.', "> This isn't a valid operator 😔", nil].join("\n")).to_stdout
+        end
+
+        it 'does not exit the flow' do
+          expect { subject }.not_to raise_error SystemExit
+        end
+      end
     end
   end
 end
