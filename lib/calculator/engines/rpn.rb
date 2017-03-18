d6a672d765124e14894aef75e5a2e20b4854e386
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-18 01:56:04 -0300

    Create a process input method for the Engine
    This method will process the current input and add to the stack if it is
    a value. if not (maybe some operation), it will check if it is possible
    to do the operation (at least 2 values on the stack), if not, raise an
    error

--- lib/calculator/engines/rpn.rb
+++ lib/calculator/engines/rpn.rb
@@ -1,4 +1,16 @@
 module Engines
   class RPN
+    def initialize
+      @stack = []
+    end
+
+    def process_input(input)
+      if input.value?
+        @stack << input.value
+        input.value
+      else
+        raise Errors::InvalidCalculation, 'Not enough values to calculate' if @stack.size < 2
+      end
+    end
   end
 end
---
+++ spec/lib/calculator/engines/rpn_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+module Calculator
+  RSpec.describe Engines::RPN do
+    let(:engine) { described_class.new }
+
+    describe '#process_input' do
+      subject { engine.process_input(input) }
+
+      context 'with a value input' do
+        let(:input) { Input.new('123') }
+
+        it 'sends the value to the stack' do
+          expect(engine.instance_variable_get(:@stack)).to be_empty
+
+          subject
+
+          expect(engine.instance_variable_get(:@stack).first).to eq(123)
+        end
+
+        it 'returns the value' do
+          expect(subject).to eq(123)
+        end
+      end
+
+      context 'with an operator' do
+        context 'and the stack is without the necessary size to calculate' do
+          before { engine.process_input(Input.new('1')) }
+
+          let(:input) { Input.new('an_operator') }
+
+          it 'raises an invlaid calculation error' do
+            expect { subject }.to raise_error(Errors::InvalidCalculation, "Not enough values to calculate")
+          end
+        end
+      end
+    end
+  end
+end
