8fe2edb33cb1b89e3a1f8149353302c51bd0ffdf
Carlos Ribeiro <mail@carlosribeiro.me>
2017-03-20 12:10:03 -0300

    Remove the multi input from the cli

--- lib/calculator/interfaces/cli.rb
+++ lib/calculator/interfaces/cli.rb
@@ -11,19 +11,15 @@ module Interfaces
     def start(_param)
       puts 'Starting calculator.'
       while stdin = $stdin.gets&.chomp
-          inputs = stdin.split(' ')
-          inputs.each do |input|
-            begin
-              # require 'pry' ; binding.pry
-              input = Input.new(input)
-              result = process_input(input)
-              puts "> #{result}"
-            rescue Errors::InvalidCalculation => e
-              puts "💥 invalid calculation. #{e.message}"
-            rescue Errors::InvalidOperator
-              puts "> This isn't a valid operator 😔"
-            end
-          end
+        begin
+          input = Input.new(stdin)
+          result = process_input(input)
+          puts "> #{result}"
+        rescue Errors::InvalidCalculation => e
+          puts "💥 invalid calculation. #{e.message}"
+        rescue Errors::InvalidOperator
+          puts "> This isn't a valid operator 😔"
+        end
       end
     end
   end
